bd459b7f62ab4b63301be15a53c4ce09c209eb89
IlyaV <ifromufa@yandex.ru>
2025-04-14 23:07:16 +0500

    Add scanner and delete function for

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name?\n" +  "Enter text: ");
+        String userName = scanner.nextLine();
+        System.out.println("Hello and welcome! " + userName);
     }
 }
